ff060fcf1a69935baad87878c6a4c708957d131e
marilaine maciel coraleski <marilainemaciel66@gmail.com>
2024-04-18 21:55:17 -0300

    meu-treino

---
+++ src/App.java
@@ -0,0 +1,21 @@
+ import java.util.Scanner;
+ public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!"); 
+
+        //booleano (falso,verdadeiro)
+        boolean morango= true; boolean morango= false;
+        //scanner(entrada de dados)
+        Scanner scanner = ()
+        //int ( numero inteiro) (int numero_inteiro = 10;)
+        //double(numero quebrado) (double temperaturaCelsius = scanner.nextDouble();)
+        // if (temperaturaCelsius < zeroAbsolutoEmCelsius)
+        //else()
+        // / ( dividir ) + (mais) -(menos) *(multipricar )
+        // && ( compra ) || (compara)
+        // a 5 = b 10  
+        // (a) é igual a (b) ? se não então sempre sera false     
+        //char (caractere) 'a'
+
+    }
+}
